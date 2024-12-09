7ff960dd43dacd3ee114028d811fbdabfb62cab6
A. Mestra <89419093+uzideath@users.noreply.github.com>
2024-12-09 15:30:41 -0500

    updated player update event

--- src/lib/structures/Client.ts
+++ src/lib/structures/Client.ts
@@ -8,7 +8,7 @@ import { Poru } from 'poru';
 import { minutesToMilliseconds } from '#lib/util';
 
 export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
-	public poru: Poru
+	public poru: Poru;
 	public constructor() {
 		super({
 			intents: [
@@ -16,7 +16,7 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 				GatewayIntentBits.GuildMessages,
 				GatewayIntentBits.DirectMessages,
 				GatewayIntentBits.MessageContent,
-				GatewayIntentBits.GuildVoiceStates
+				GatewayIntentBits.GuildVoiceStates,
 			],
 			partials: [Partials.Channel],
 			presence: {
@@ -35,7 +35,7 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 		this.ownerIds = [''];
 		this.restDebug = false;
 
-		this.poru = new Poru(this, config.nodes, config.options)
+		this.poru = new Poru(this, config.nodes, config.options);
 
 		this.registerPoruEvents();
 	}
@@ -55,7 +55,7 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 
 		const promiseString = await super.login(token);
 		this.logger.info(`Logged in as ${cyanBright(underline(`${this.user?.tag}`))}`);
-		this.poru.init()
+		this.poru.init();
 		await initiateCommands(this as Client, {
 			register: false,
 			sync: false,
@@ -66,20 +66,20 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 	}
 	private registerPoruEvents() {
 		const disconnectTimers = new Map<string, NodeJS.Timeout>();
-	
+
 		this.poru.on('trackStart', (player, track) => {
 			const channel = this.channels.cache.get(player.textChannel) as TextChannel;
 			channel?.send(`Now playing \`${track.info.title}\``);
-	
+
 			if (disconnectTimers.has(player.guildId)) {
 				clearTimeout(disconnectTimers.get(player.guildId)!);
 				disconnectTimers.delete(player.guildId);
 			}
 		});
-	
+
 		this.poru.on('queueEnd', async (player) => {
 			const channel = this.channels.cache.get(player.textChannel) as TextChannel;
-	
+
 			if (player.isAutoPlay) {
 				try {
 					await player.autoplay();
@@ -89,7 +89,7 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 				}
 				return;
 			}
-	
+
 			if (!player.isPlaying && player.queue.size === 0) {
 				const disconnectTimer: NodeJS.Timeout = setTimeout(() => {
 					if (!player.isPlaying && player.queue.size === 0) {
@@ -98,9 +98,9 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 						disconnectTimers.delete(player.guildId);
 					}
 				}, minutesToMilliseconds(3)) as NodeJS.Timeout;
-	
+
 				disconnectTimers.set(player.guildId, disconnectTimer);
-	
+
 				const onStatusChange = () => {
 					if (player.isPlaying || player.queue.size > 0) {
 						clearTimeout(disconnectTimer);
@@ -108,44 +108,42 @@ export class Client<Ready extends boolean = true> extends DJSClient<Ready> {
 						this.poru.off('playerUpdate', onStatusChange);
 					}
 				};
-	
+
 				this.poru.on('playerUpdate', onStatusChange);
 			}
 		});
-	
+
 		this.poru.on('nodeDisconnect', (node) => {
 			this.logger.warn(`Node ${node.name} has been disconnected.`);
 		});
-	
+
 		this.poru.on('nodeError', (node, error) => {
 			this.logger.warn(`Node ${node.name} just fired an error: ${error}`);
 		});
-	
+
 		this.poru.on('trackError', (player, track, error) => {
 			const channel = this.channels.cache.get(player.textChannel) as TextChannel;
 			console.error(`Track exception for ${track.info.title}: ${error}`);
-			channel?.send(
-				`An error occurred while playing \`${track.info.title}\`: ${error}. Skipping...`
-			);
+			channel?.send(`An error occurred while playing \`${track.info.title}\`: ${error}. Skipping...`);
 			player.destroy();
 		});
-	
+
 		this.poru.on('playerCreate', (player) => {
 			this.logger.info(`A new player was created on ${player.guildId}`);
 		});
-	
+
 		this.poru.on('playerDestroy', (player) => {
 			this.logger.info(`A player was destroyed on ${player.guildId}`);
 		});
-	
+
 		this.poru.on('playerUpdate', (player) => {
-			this.logger.info(`Player updated in guild: ${player.guildId}`);
+			//	this.logger.info(`Player updated in guild: ${player.guildId}`);
 		});
-	
+
 		this.poru.on('nodeConnect', (node) => {
 			this.logger.debug(`Node ${node.name} connected successfully.`);
 		});
-	
+
 		this.poru.on('nodeReconnect', (node) => {
 			this.logger.info(`Node ${node.name} is reconnecting...`);
 		});
